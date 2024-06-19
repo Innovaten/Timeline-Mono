cab1a1717bb1c1af2bf636cafb7645043781288b
Raphael <raphmens2018@gmail.com>
2024-06-19 22:30:28 +0000

    Created the filters. Not properly working

--- apps/lms/src/routes/announcements.lazy.tsx
+++ apps/lms/src/routes/announcements.lazy.tsx
@@ -1,16 +1,105 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+import { AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai'
 
 export const Route = createLazyFileRoute('/announcements')({
   component: () => <Announcements />
 })
 
+
+
+const announcememnts = [
+  {
+    title: 'Assignment Update',
+    class: 'SAT SEPT 2024',
+    date: '17:25 - 4 Jun',
+  },
+  {
+    title: 'SAT Trial Quiz',
+    class: 'SAT SEPT 2024',
+    date: '11:32 - 2 Jun',
+  },
+  {
+    title: 'New Assignment',
+    class: 'SAT AUG 2024',
+    date: '12:47 - 25 May',
+  },
+  {
+    title: 'Farewell Announcement',
+    class: 'SAT MAR 2024',
+    date: '8:24 - 29 Mar',
+  },
+]
+
+  const dropdownItems = [
+  {
+    option1: 'Today',
+    option2: 'Last Week',
+    option3: 'Last Month'
+  },
+]
+
 function Announcements({}){
 
   return (
     <>
-      Hi from Annoucements. <br />
-      Implement Me.
+     {/* Title */}
+     <div>
+      <h1 className='text-blue-900'>Announcements</h1>
+     </div>
+     {/* Filters */}
+     <div className='flex justify-between'>
+      <Dropdown buttonText = 'All Announcements' options = {dropdownItems}/>
+      <div className='flex'>
+        <Dropdown buttonText = 'All Classes' options = {dropdownItems}/>
+        <Dropdown buttonText = 'This Year' options = {dropdownItems}/>
+      </div>
+     </div>
+     {/* Main Announcements */}
+     <div className='border-4 border-blue-50 rounded-md flex-1 px-1' >
+        {announcememnts.map(( announcememnt, index) => (
+        <div key = {index} className='flex items-center p-1 h-15 bg-blue-50 justify-between mx-1 mt-1 mb-1'>
+          <div className='w-9/12'>
+            <h5 className='ml-2 text-blue-800'>{announcememnt.title}</h5>
+          </div>
+          <div className='justify-between w-3/12 mr-2 items-center flex'>
+            <span className='text-blue-800'>{announcememnt.class}</span>
+            <span className='text-blue-800'>{announcememnt.date}</span>
+            <div className='w-1.5 h-1.5 bg-blue-600 rounded-full'></div>
+          </div>
+      </div>
+      ))}
+     </div>
+     <div className='flex justify-end mt-1' >
+      <span className='text-xs text-blue-600 justify-end'>Showing <span className='text-xs text-blue-800 font-bold'>{(announcememnts.length)}</span> out of <span className='text-xs text-blue-800 font-bold'>10</span></span>
+     </div>
     </>
   )
 
+}
+
+
+function Dropdown({buttonText, options}){
+  const [isOpen, setIsOpen] = useState(false)
+  return(
+    <div className='items-center my-5'>
+     <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className={`pl-3 pr-3 w-[180px] text-white font-bold items-center justify-between flex ${
+          !isOpen ? 'bg-blue-500 border-blue-900 rounded-full' : 'bg-blue-500 rounded-tl-lg rounded-tr-lg'
+        }`}
+      >
+        {buttonText}
+        {isOpen ? <AiOutlineCaretUp className='h-8'/> : <AiOutlineCaretDown className='h-8'/>}
+      </button>
+
+    {isOpen && (
+      <div className=' bg-blue-500 w-[180px] items-center flex-col justify-around absolute mt-0.25 rounded-br-lg rounded-bl-lg mb-15'>
+          <h4  className='pb-2 flex justify-center text-white text-sm'>{options.option1}</h4>
+          <h4  className='pb-2 flex justify-center text-white text-sm'>{options.option2}</h4>
+          <h4  className='pb-2 flex justify-center text-white text-sm'>{options.option3}</h4>
+      </div>
+    )}
+    </div>
+  )
 }
--- apps/services/src/handlers/notifications/send-email.ts
+++ apps/services/src/handlers/notifications/send-email.ts
@@ -8,7 +8,7 @@ type LMSKafkaMessage = {
     purpose: string,
     data: Record<string, any>
 }
-const mg = new Mailgun(FormData).client({ username: 'api', key: config.mail.mailgun_key})
+// const mg = new Mailgun(FormData).client({ username: 'api', key: config.mail.mailgun_key})
 
 export async function emailHandler(KafkaArgs: EachMessagePayload){
     try {
@@ -31,14 +31,14 @@ type RequestToMGArgs = {
 }
 
 async function sendRequestToMG(args: RequestToMGArgs) {
-    mg.messages.create(config.mail.mailgun_domain, { // Change to real account when in prod
-        to: [args.email],
-        from: 'Timeline Trust Support <noreply-timeline-support@mg.kodditor.co>',
-        bcc: ['kobbyowusudarko@gmail.com'],
-        subject: args.subject,
-        html: `
-        <html>
-          ${args.body}
-        </html>
-        `})
+    // mg.messages.create(config.mail.mailgun_domain, { // Change to real account when in prod
+    //     to: [args.email],
+    //     from: 'Timeline Trust Support <noreply-timeline-support@mg.kodditor.co>',
+    //     bcc: ['kobbyowusudarko@gmail.com'],
+    //     subject: args.subject,
+    //     html: `
+    //     <html>
+    //       ${args.body}
+    //     </html>
+    //     `})
 }

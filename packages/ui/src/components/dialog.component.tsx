432074ccc51afc53bf7c217c399f673c084b04ce
gene-999 <eugenebaidoo5@gmail.com>
2024-06-23 01:36:47 +0000

    feat: added background blur to diag box

--- packages/ui/src/components/dialog.component.tsx
+++ packages/ui/src/components/dialog.component.tsx
@@ -1,4 +1,4 @@
-import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Description, Dialog, DialogPanel, DialogTitle, useClose } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/solid';
 import { ReactNode } from 'react';
 
@@ -14,14 +14,14 @@ type DialogProps = {
 export default function DialogContainer({ isOpen, toggleOpen, onClose, title, description, children }: DialogProps){
     return (
         <>
-          <Dialog open={isOpen} onClose={onClose ?? function(){} } className="relative z-50">
-            <div className="fixed inset-0 flex w-screen bg-slate-200/70 items-center justify-center p-4">
-              <DialogPanel className="sm:min-w-[600px] text-blue-800 max-w-lg relative space-y-3 border bg-white p-12 rounded shadow">
-                <span onClick={toggleOpen} className='grid place-items-center w-6 aspect-square rounded-full bg-blue-50 text-blue-700 hover:bg-blue-100 cursor-pointer duration-150 top-2 right-2 absolute'><XMarkIcon className='w-4' /></span>
+          <Dialog open={isOpen} onClose={onClose ?? function(){}} className="relative z-50">
+            <div className="fixed inset-0 flex w-screen  backdrop-blur-[2px] items-center justify-center p-4">
+              <DialogPanel className="sm:min-w-[600px] text-blue-800 max-w-lg relative space-y-3 border bg-white p-12 rounded-lg shadow">
+                <span onClick={useClose} className='grid place-items-center w-6 aspect-square rounded-full bg-blue-50 text-blue-700 hover:bg-blue-100 cursor-pointer duration-150 top-2 right-2 absolute'><XMarkIcon className='w-4' /></span>
                 <DialogTitle className="font-semibold text-2xl">{title}</DialogTitle>
                 <Description>{description}</Description>
                 <div>
-                  {children}
+                  {children}packages/ui/src/components/dialog.component.tsx
                 </div>
               </DialogPanel>
             </div>

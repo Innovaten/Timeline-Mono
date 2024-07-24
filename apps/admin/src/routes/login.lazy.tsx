d9575ae39adb61c13d70903b103db3d22fbed059
Kelly Buabeng <buabengkelly@gmail.com>
2024-07-24 03:50:48 +0000

    Fixed the error in routes

--- apps/admin/src/routes/login.lazy.tsx
+++ apps/admin/src/routes/login.lazy.tsx
@@ -384,7 +384,7 @@ function TwoFactorAuthentication({ componentRef }: PageProps){
        function handleSubmit(values: { email: string }){
           multiPage.goToNext();
           toggleLoading();
-          makeUnauthenticatedRequest('get', `/api/v1/auth/forgot-password?email=${values.email}`)
+          makeUnauthenticatedRequest('get', `/api/v1/auth/otp/send/forgot-password?email=${values.email}`)
            .then(res => {
               if(res.data.success){
                 sessionStorage.setItem('e', values.email);
@@ -473,7 +473,7 @@ function ForgotVerification({componentRef, multiPage}: PageProps){
       multiPage.goToNext()
       
 
-      makeUnauthenticatedRequest('get', `/api/v1/auth/verify-otp?email=${email}&otp=${OTP}`)
+      makeUnauthenticatedRequest('get', `/api/v1/auth/otp/verify-otp?email=${email}&otp=${OTP}`)
       .then( res => {
           if(res.data.success){
               sessionStorage.setItem('o', OTP);

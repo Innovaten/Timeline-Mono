0b0ff37f3d8b41b41fb16121ed02d2af45ff0cce
gene-999 <eugenebaidoo5@gmail.com>
2024-07-30 17:28:46 +0000

    fix: add assigned service to providers and exports

--- apps/core/src/common/index.ts
+++ apps/core/src/common/index.ts
@@ -3,10 +3,11 @@ import { JwtService } from "./services/jwt.service";
 import { UserService } from "./services/user.service";
 import { AuthGuard } from "./guards/jwt.guard";
 import { KafkaService } from "./services/kafka.service";
+import { AssignedService} from "./services/assigned.service";
 
 @Module({
-    providers: [JwtService, UserService, KafkaService, AuthGuard],
-    exports: [JwtService, UserService, AuthGuard]
+    providers: [JwtService, UserService, KafkaService, AssignedService, AuthGuard],
+    exports: [JwtService, UserService, AssignedService, AuthGuard]
 })
 
 export default class CustomCommon {}

2a85e0746c293f177a49ec35c5ebf5675591d459
gene-999 <eugenebaidoo5@gmail.com>
2024-07-30 17:24:15 +0000

    fix: typo

--- apps/core/src/modules/module.module.ts
+++ apps/core/src/modules/module.module.ts
@@ -3,12 +3,14 @@ import { ModulesController } from './module.controller';
 import { ModulesService } from './module.service';
 import { AuthGuard } from '../common/guards/jwt.guard';
 import { JwtService } from '../common/services/jwt.service';
+import { AssignedService } from '../common/services/assigned.service';
 
 @Module({
   providers: [
     ModulesService,
     AuthGuard,
     JwtService,
+    AssignedService,
   ],
   controllers: [ModulesController],
 })

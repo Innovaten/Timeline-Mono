b2179388578d20ccdea3f5e0a9005bfb9e2976d3
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-08-22 08:10:53 +0000

    Added services deployment

--- apps/services/src/index.ts
+++ apps/services/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import { start } from "./start";
 import { ServicesConfig } from "../config";
+import http from 'node:http'
 
 export type LMSKafkaMessage = {
     purpose: string,
@@ -15,10 +16,27 @@ export type LMSKafkaMessage = {
         })
 
         start();
-
     } catch (err) {
         console.log("--- Services Error ---")
         console.log(err)
+        console.log("*** Auto-Restarting ***")
+        index()
+    }
+    finally {
+
+        const server = http.createServer(
+            (req, res) => {
+                res.writeHead(200),
+                res.end("ping:pong")
+            }
+        )
+
+        const PORT = 3000
+        const HOST = '0.0.0.0'
+
+        server.listen(PORT, HOST, () => {
+            console.log(`Services is running. Check health at http(s)://${HOST}:${PORT}`)
+        })
     }
 
 })()

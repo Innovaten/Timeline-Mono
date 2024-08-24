887fdd67aeadd6f5cd52408373fef4771f013155
gene-999 <eugenebaidoo5@gmail.com>
2024-08-24 21:14:25 +0000

    improved ui/ux

--- apps/lms/src/hooks/announcements.hooks.ts
+++ apps/lms/src/hooks/announcements.hooks.ts
@@ -63,3 +63,54 @@ export function useAnnouncement(refreshFlag: boolean = true, specifier: string, 
     return { isLoading, announcement }
 
 }
+
+export function useAnnouncementsByClass(refreshFlag: boolean = true, specifier: string, isId:boolean = true){
+
+    const [ announcements, setAnnouncements ] = useState<(IAnnouncementDoc & { createdBy: IUserDoc, updatedBy:IUserDoc })[] | null>(null)
+    const { isLoading, toggleLoading, resetLoading } = useLoading()
+
+    useEffect( () => {
+        abstractAuthenticatedRequest(
+            "get",
+            `/api/v1/classes/${specifier}/announcements/?isId=${isId}`,
+            {},
+            {},
+            {
+                onSuccess: (data) => setAnnouncements(data),
+                onFailure: (err) => {toast.error(`${err.msg}`)},
+                finally: resetLoading
+            }
+        )
+
+    }, [refreshFlag])
+
+
+    return { isLoading, announcements }
+
+}
+
+export function useAnnouncementsCount(classCode: string){
+    const [ isLoading, setPendingIsLoading ] = useState<boolean>(true);
+    const [ count, setCount ] = useState<number>(0);
+
+    useEffect(
+        () =>{
+            setPendingIsLoading(true);
+            makeAuthenticatedRequest(
+                "get",
+                `/api/v1/modules/${classCode}/count?filter=${JSON.stringify({})}`
+            )
+            .then( res => {
+                if(res.status == 200 && res.data.success){
+                    setCount(res.data.data);
+                } else {
+                    console.log(res.data.error.msg);
+                    toast.error(res.data.error.msg);
+                }
+                setPendingIsLoading(false);
+            })
+        }, []
+)
+
+    return { isLoading, count }
+}
